e4f51a5fc616610cad711787b2100c5f18a644ed
mohamed.sayed <mohamed.sayed.salah@outlook.com>
2021-03-23 16:29:12 +0200

    remove unused header

--- src/Header.js
+++ src/Header.js
@@ -1,30 +1,7 @@
-import {Pane, Text, SidebarTab, Paragraph, Tablist, Tab} from 'evergreen-ui'
 import React, {useState} from "react";
 import "./Jobs/jobsStyle.css"
-import {Link, BrowserRouter} from "react-router-dom";
 
-const Header = ({setSelectedTab}) => {
-    const tabs = ['Your Jobs', 'New Job']
-    const [selectedIndex, setSelectedIndex] = useState(0)
-    // return <div className={"topBar"}>
-    //         <div className={"topbar-navigation"}>
-    //         {tabs.map((tab, index) => (
-    //             <Tab
-    //                 key={tab}
-    //                 id={tab}
-    //                 onSelect={() => setSelectedIndex(index)}
-    //                 isSelected={index === selectedIndex}
-    //                 aria-controls={`panel-${tab}`}
-    //             >
-    //                 <Link className={"navi"} to={tab === 'Your Jobs' ? '/jobs' : '/jobs/new'}>
-    //                     <div >{tab}</div>
-    //                 </Link>
-    //             </Tab>
-    //         ))}
-    //         </div>
-    // </div>
-
-
+const Header = () => {
     return <nav className="bg-gray-800">
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
@@ -97,19 +74,6 @@ const Header = ({setSelectedTab}) => {
             </div>
         </div>
 
-        <div className="sm:hidden" id="mobile-menu">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-
-                <a href="#"
-                   className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
-                <a href="#"
-                   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
-                <a href="#"
-                   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-                <a href="#"
-                   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
-            </div>
-        </div>
     </nav>
 
 
